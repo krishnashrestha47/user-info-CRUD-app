85d8dddc1b1c55d3edc6e45c1183ab4cf537d3ed
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-07-10 22:19:06 +1000

    schema created for user details

---
+++ api/src/models/userSchema.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  mobile: {
+    type: Number,
+    required: true,
+  },
+  work: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+export const users = new mongoose.model("Users", userSchema);
