127a1f0b7a833beebe5c33f69e5bddf1ac063c9a
Krishna Shrestha <krish_sth2005@yahoo.com>
2022-08-14 23:05:55 +1000

    pages wrapped with default layout

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,48 +1,51 @@
 import React from "react";
 import { Button, Container, Table } from "react-bootstrap";
+import DefaultLayout from "../layout/DefaultLayout";
 
 export const Home = () => {
   return (
-    <div className="mt-5">
-      <Container>
-        <div className="add_btn mt-2 text-end">
-          <Button>Add Data</Button>
-        </div>
-        <Table striped bordered hover className="mt-5">
-          <thead>
-            <tr className="table-warning">
-              <th>Id</th>
-              <th>Username</th>
-              <th>Email</th>
-              <th>Job</th>
-              <th>Number</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>mark@gmail.com</td>
-              <td>Software Developer</td>
-              <td>1234567890</td>
-              <td>
-                <Button className="btn-success">
-                  <i className="fa-solid fa-eye"></i>
-                </Button>{" "}
-                {""}
-                <Button className="btn-primary">
-                  <i className="fa-solid fa-pen-to-square"></i>
-                </Button>{" "}
-                {""}
-                <Button className="btn-danger">
-                  <i className="fa-solid fa-trash-can"></i>
-                </Button>
-              </td>
-            </tr>
-          </tbody>
-        </Table>
-      </Container>
-    </div>
+    <DefaultLayout>
+      <div className="mt-5">
+        <Container>
+          <div className="add_btn mt-2 text-end">
+            <Button>Add Data</Button>
+          </div>
+          <Table striped bordered hover className="mt-5">
+            <thead>
+              <tr className="table-warning">
+                <th>Id</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Job</th>
+                <th>Number</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>mark@gmail.com</td>
+                <td>Software Developer</td>
+                <td>1234567890</td>
+                <td>
+                  <Button className="btn-success">
+                    <i className="fa-solid fa-eye"></i>
+                  </Button>{" "}
+                  {""}
+                  <Button className="btn-primary">
+                    <i className="fa-solid fa-pen-to-square"></i>
+                  </Button>{" "}
+                  {""}
+                  <Button className="btn-danger">
+                    <i className="fa-solid fa-trash-can"></i>
+                  </Button>
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+        </Container>
+      </div>
+    </DefaultLayout>
   );
 };
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Container, Form, Row } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import DefaultLayout from "../layout/DefaultLayout";
 
 const initialState = {
   name: "",
@@ -23,95 +24,97 @@ export const Register = () => {
   };
 
   return (
-    <Container>
-      <NavLink to="/">Home</NavLink>
+    <DefaultLayout>
+      <Container>
+        <NavLink to="/">Home</NavLink>
 
-      <Form className="mt-5">
-        <Row>
-          <Form.Group
-            className="mb-4 col-lg-6 col-md-6 col-12"
-            controlId="formGroupEmail"
-          >
-            <Form.Label>Name</Form.Label>
-            <Form.Control
-              onChange={handleOnChange}
-              name="name"
-              type="email"
-              placeholder="Enter name"
-            />
-          </Form.Group>
-          <Form.Group
-            className="mb-4 col-lg-6 col-md-6 col-12"
-            controlId="formGroupEmail"
-          >
-            <Form.Label>Email address</Form.Label>
-            <Form.Control
-              onChange={handleOnChange}
-              name="email"
-              type="email"
-              placeholder="Enter email"
-            />
-          </Form.Group>
-          <Form.Group
-            className="mb-4 col-lg-6 col-md-6 col-12"
-            controlId="formGroupPassword"
-          >
-            <Form.Label>Age</Form.Label>
-            <Form.Control
-              onChange={handleOnChange}
-              name="age"
-              type="number"
-              placeholder="Enter age"
-            />
-          </Form.Group>
-          <Form.Group
-            className="mb-4 col-lg-6 col-md-6 col-12"
-            controlId="formGroupPassword"
-          >
-            <Form.Label>Mobile</Form.Label>
-            <Form.Control
-              onChange={handleOnChange}
-              name="mobile"
-              type="number"
-              placeholder="Enter mobile number"
-            />
-          </Form.Group>
-          <Form.Group
-            className="mb-4 col-lg-6 col-md-6 col-12"
-            controlId="formGroupPassword"
-          >
-            <Form.Label>Work</Form.Label>
-            <Form.Control
-              onChange={handleOnChange}
-              name="work"
-              type="text"
-              placeholder="Enter work"
-            />
-          </Form.Group>
-          <Form.Group
-            className="mb-4 col-lg-6 col-md-6 col-12"
-            controlId="formGroupEmail"
-          >
-            <Form.Label>Address</Form.Label>
-            <Form.Control
-              onChange={handleOnChange}
-              name="address"
-              type="text"
-              placeholder="Enter address"
-            />
-          </Form.Group>
-          <Form.Group className="mb-4" controlId="formGroupPassword">
-            <Form.Label>Description</Form.Label>
-            <Form.Control
-              onChange={handleOnChange}
-              name="description"
-              as="textarea"
-              rows={5}
-              placeholder="Your text here"
-            />
-          </Form.Group>
-        </Row>
-      </Form>
-    </Container>
+        <Form className="mt-5">
+          <Row>
+            <Form.Group
+              className="mb-4 col-lg-6 col-md-6 col-12"
+              controlId="formGroupEmail"
+            >
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                onChange={handleOnChange}
+                name="name"
+                type="email"
+                placeholder="Enter name"
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-4 col-lg-6 col-md-6 col-12"
+              controlId="formGroupEmail"
+            >
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                onChange={handleOnChange}
+                name="email"
+                type="email"
+                placeholder="Enter email"
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-4 col-lg-6 col-md-6 col-12"
+              controlId="formGroupPassword"
+            >
+              <Form.Label>Age</Form.Label>
+              <Form.Control
+                onChange={handleOnChange}
+                name="age"
+                type="number"
+                placeholder="Enter age"
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-4 col-lg-6 col-md-6 col-12"
+              controlId="formGroupPassword"
+            >
+              <Form.Label>Mobile</Form.Label>
+              <Form.Control
+                onChange={handleOnChange}
+                name="mobile"
+                type="number"
+                placeholder="Enter mobile number"
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-4 col-lg-6 col-md-6 col-12"
+              controlId="formGroupPassword"
+            >
+              <Form.Label>Work</Form.Label>
+              <Form.Control
+                onChange={handleOnChange}
+                name="work"
+                type="text"
+                placeholder="Enter work"
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-4 col-lg-6 col-md-6 col-12"
+              controlId="formGroupEmail"
+            >
+              <Form.Label>Address</Form.Label>
+              <Form.Control
+                onChange={handleOnChange}
+                name="address"
+                type="text"
+                placeholder="Enter address"
+              />
+            </Form.Group>
+            <Form.Group className="mb-4" controlId="formGroupPassword">
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                onChange={handleOnChange}
+                name="description"
+                as="textarea"
+                rows={5}
+                placeholder="Your text here"
+              />
+            </Form.Group>
+          </Row>
+        </Form>
+      </Container>
+    </DefaultLayout>
   );
 };
